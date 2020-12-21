b4d0214c7dd938fd10f75b308b4668a56304652c
Tabea David <info@justb3a.com>
2020-12-21 22:37:39 +0100

    Set macOS deployment targtet to version 10.14

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "LightCycle",
-	platforms: [.macOS(.v10_15)]
+	platforms: [.macOS(.v10_14)]
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
